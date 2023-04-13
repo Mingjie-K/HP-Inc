4ac44cdaf66ce9f1eff6aede7f19dbbd6ebc5386
Koh <mansred.koh@hp.com>
2023-04-13 13:21:37 +0800

    Added a new user variable path in system

--- SAIL/Code/SAIL.bat
+++ SAIL/Code/SAIL.bat
@@ -1,4 +1,4 @@
-call %UserProfile%\Anaconda3\Scripts\activate.bat
+call %ACTIVATE_PATH%\activate.bat
 call python "%UserProfile%\HP Inc\PrintOpsDB - DB_DailyOutput\Code\Email_CSV_SP.py"
 timeout 5
 call conda activate SAIL
