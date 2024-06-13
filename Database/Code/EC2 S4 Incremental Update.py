4d5014d1ab9017aa7b83bfae787a4a91f5be948c
Mingjie-K <mj.koh@hotmail.sg>
2024-06-13 10:48:07 +0800

    Comment out secret manager function

--- Database/Code/EC2 S4 Incremental Update.py
+++ Database/Code/EC2 S4 Incremental Update.py
@@ -162,25 +162,25 @@ def db_insertdata(df, table, engine, col_dtype, if_exist):
         print('Insertion of data to Database table {} FAILED!'.format(table))
 
 
-def get_secret(secret_name):
-    # Create a Secrets Manager client
-    client = boto3.client('secretsmanager', region_name='us-west-2')
+# def get_secret(secret_name):
+#     # Create a Secrets Manager client
+#     client = boto3.client('secretsmanager', region_name='us-west-2')
 
-    try:
-        # Attempt to retrieve the secret value
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name)
-    except ClientError as e:
-        # Handle the exception if the secret can't be retrieved
-        raise e
+#     try:
+#         # Attempt to retrieve the secret value
+#         get_secret_value_response = client.get_secret_value(
+#             SecretId=secret_name)
+#     except ClientError as e:
+#         # Handle the exception if the secret can't be retrieved
+#         raise e
 
-    # If there's no exception, process the retrieved secret
-    if 'SecretString' in get_secret_value_response:
-        secret = get_secret_value_response['SecretString']
-    else:
-        # For binary secrets, decode them before using
-        secret = get_secret_value_response['SecretBinary'].decode('utf-8')
-    return secret
+#     # If there's no exception, process the retrieved secret
+#     if 'SecretString' in get_secret_value_response:
+#         secret = get_secret_value_response['SecretString']
+#     else:
+#         # For binary secrets, decode them before using
+#         secret = get_secret_value_response['SecretBinary'].decode('utf-8')
+#     return secret
 
 
 def establish_sharepoint_connection(client_id, client_secret):
